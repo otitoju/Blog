e84ef5910cf77e71ba619a296e07c1c80ebee80f
otitoju <otitojuoluwapelumi@gmail.com>
2018-07-26 17:51:16 -0700

    added post controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -3,5 +3,20 @@ const post = require('../models/post')
 
 // create post
 exports.createNewPost = async (req, res) => {
-    
+    const body = req.body
+    if(!body.title || !body.content || !body.author){
+        res.status(400).json({message:`Please ensure all fields are filled`})  
+    }
+    else {
+        const Post = await post.create({
+            title:body.title,
+            content:body.content,
+            time:body.time,
+            author:body.author
+        })
+        res.status(200).json({
+            message:`post created successfully`,
+            post:Post
+        })
+    }
 }
